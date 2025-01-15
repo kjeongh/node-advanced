f71d972bc42810e496f9ee993991ab54a8744164
kjeongh <wellsy001012@gmail.com>
2025-01-16 00:39:18 +0900

    broadcast via socket

--- src/chat/client.ts
+++ src/chat/client.ts
@@ -1,4 +1,28 @@
 import net from 'net';
+import { Writable } from 'stream';
+
+const log = (message: string) => {
+    process.stdout.write(`/r${message}`);
+}
+
+const myWritable = new Writable({
+    write(chunk, encoding, callback) {
+        const data = JSON.parse(chunk);
+        const id = data.id;
+        const message = data.message;
+
+        if(message) {
+            log(`${id} says: ${message}`);
+        } else {
+            // 최초 연결시 메세지 없으므로 자신의 id 출력
+            log(`My ID: ${id}`);
+        }
+        
+        log('Type something...:');
+
+        callback(null, chunk);
+    },
+})
 
 // 입력한것이 net서버를 통해 다시 출력됨
-process.stdin.pipe(net.connect(3000)).pipe(process.stdout);
+process.stdin.pipe(net.connect(3000)).pipe(myWritable);
--- src/chat/server.ts
+++ src/chat/server.ts
@@ -1,11 +1,38 @@
 import { randomUUID } from 'crypto';
+import EventEmitter from 'events';
 import net, { type Socket } from 'net';
+import { Writable } from 'stream';
 
+// 현재 접속한 클라이언트 목록
 const clients = new Map();
+
+// 전송자를 제외한 접속자에게 브로드캐스트
+const broadCastToClients = (senderSocketId, data) => {
+    [...clients.values()]
+        .filter((clientSocket) => clientSocket.id !== senderSocketId)
+        .forEach((clientSocket) => {
+            clientSocket.write(data);
+        })
+}
+
+const broadCastViaStream: (socket: Socket) => Writable = (socket: Socket) => {
+    return new Writable({
+        write(chunk, encoding, callback) {
+            const data = JSON.stringify({
+                message: chunk.toString(),
+                id: socket.id.slice(0, 5)
+            })
+
+            broadCastToClients(socket.id, data);
+
+            callback(null, chunk);
+        }
+    })
+}
 
 // net - TCP기반 서버/클라이언트 통신을 위한 비동기 API 제공
 const server = net.createServer((socket) => {
-    socket.pipe(socket)
+    socket.pipe(broadCastViaStream(socket));
 })
 
 server.on('connection', (socket: Socket) => {
@@ -19,7 +46,7 @@ server.on('connection', (socket: Socket) => {
     }))
 })
 
-server.on('close', (socket) => {
+server.on('close', (socket: Socket) => {
     console.log(`${socket.id} clients disconnected`);
 })
 
